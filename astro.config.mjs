5a99e19b9e7398e9278bd16d0021ee5056be1b6f
Joseph Muller <jtmuller5@gmail.com>
2024-03-19 20:22:39 -0400

    Docs on the rocks

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'COTR Snippets',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Snippets',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'cotrPrint', link: '/cotrprint' },
+					],
+				},
+				{
+					label: 'Languages',
+					autogenerate: { directory: 'languages' },
+				},
+			],
+		}),
+	],
+});
